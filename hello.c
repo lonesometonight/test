eee91105c70462e589962c6f2707b077d60b044e
dev8 <dev8@server.com>
2021-10-13 22:57:23 +0800

    master

---
+++ hello.c
@@ -0,0 +1,13 @@
+/*
+ * @Author: user
+ * @Date: 2021-10-13 22:55:59
+ * @LastEditors: user
+ * @LastEditTime: 2021-10-13 22:55:59
+ * @Descripttion: 
+ */
+#include <stdio.h>
+int main()
+{
+    printf("hello!!");
+    return 0;
+}
